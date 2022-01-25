878d891af5c0aff4050f2462c302b7bf196e5cd7
prynswho <priyanshu27022003@gmail.com>
2022-01-26 02:52:07 +0530

    initial commit

--- one.cpp
+++ one.cpp
@@ -1,8 +1,90 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+
+// https://www.hackerrank.com/challenges/append-and-delete/problem?isFullScreen=true
+string appendAndDelete(string s, string t, int k)
 {
-    /* code */
-    return 0;
+    if (s == t)
+        return "Yes";
+    int last;
+    int count = 0;
+    string delta;
+    if (s.size() > t.size())
+        last = t.size();
+    else
+        last = s.size();
+    int check = 0;
+    while (check < s.size() && check < t.size())
+    {
+        if (s[check] != t[check])
+            break;
+        check++;
+    }
+    cout << check << endl;
+    if (check == 0)
+    {
+        if (s.size() + t.size() <= k)
+            return "Yes";
+    }
+    int i = s.size() - check;
+    cout << i << endl;
+
+    int j = t.size() - check;
+    cout << j << endl;
+    int kj = i + j;
+    cout << kj << endl;
+    if (kj == k)
+        return "Yes";
+    else
+        return "No";
 }
+
+
+
+//https://www.hackerrank.com/challenges/append-and-delete/submissions/code/250876171
+
+string appendAndDelete(string s, string t, int k) {
+    int cl=0;
+    while(cl<s.size() && cl<t.size()){
+        if(s[cl]!=t[cl]) break;
+        cl++;
+    }
+    if(s.size()-cl+t.size()-cl<=k&& (s.size()-cl+t.size()-cl)%2==k%2){
+        return "Yes";
+    }
+    else if(s.size()+t.size()<=k){
+        return "Yes";
+    }
+    else return "No";
+    return 0;
+    
+}
+
+//https://www.hackerrank.com/challenges/repeated-string/submissions/code/248355542
+
+long repeatedString(string s, long n) {
+    string str;
+    char find = 'a';
+    int count = 0;
+    int len = s.size();
+    long nos = floor((n/len));
+    for (char i:s){
+        if (i == find) count ++;
+    }
+    long countReal = count * nos;
+    long f = n - (nos * len); 
+    for (long i = 0; i < f; i++){
+        if (s[i] == find) countReal++;
+    }
+    return countReal;
+
+}
+
+
+
+
+
+
+
+
